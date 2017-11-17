5827f2b3f6e799bdccaa2e39d6fca571ecd31335
Mohsen Ibrahim <mohsen47@hotmail.co.uk>
2017-11-17 09:32:20 +0200

    Use mohsenSy.rabbitmq role to install rabbitmq server
    I also added running composer to deploy script
    Added a new test route

--- test_app/src/AppBundle/Controller/DefaultController.php
+++ test_app/src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -17,5 +18,16 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    /**
+    * @Route("/test", name="test")
+    */
+    public function testAction(Request $request)
+    {
+      
+      return new Response(
+        "<html><body>Test Page</body></html>"
+      );
     }
 }
